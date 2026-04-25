f7ced707f2dcb2a07aa49038e83cb18c05e9bcdb
Christian <christian@flores.me>
2026-04-25 22:04:08 +0000

    autofix: preflight retry 1 for run 24910771170

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/WireGuard/wireguard-apple",
-            from: "1.0.15-26"
+            from: "1.0.15"
         ),
     ],
     targets: [
